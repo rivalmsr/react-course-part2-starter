bbd13d6cb72d5c108292cb898d96905ff8889af6
rival msr <rivalmusripal@gmail.com>
2025-01-26 15:11:42 +0700

    feat(state management): add a simple zustand devtools

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,17 @@
 import LoginStatus from './auth/LoginStatus';
+import useCounterStore from './counter/store';
 import { useTasks } from './task/TaskList';
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  const counter = useCounterStore(s => s.counter);
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <div>
+        <span className="badge text-bg-secondary m-2">counter: {counter}</span>
+        <span className="badge text-bg-secondary">{tasks.length}</span>
+      </div>
       <LoginStatus />
     </nav>
   );
